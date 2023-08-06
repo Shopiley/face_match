de51a6f50ba6e0160ec610d603b2f0e240e2f890
Fadekemi Adebayo <82163647+Shopiley@users.noreply.github.com>
2023-08-06 19:41:02 +0100

    fixed cors error

--- app/app.py
+++ app/app.py
@@ -6,14 +6,10 @@ from fastapi import FastAPI, HTTPException, status
 
 app = FastAPI()
 
-origins = [
-    "http://localhost"
-    "*"
-]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
